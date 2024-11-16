22bc9b29e6977673de358dbb9e9cf6677527b4de
Viktoria-jpg <black_tea8@bk.ru>
2024-11-16 22:37:38 +0300

    Add files via upload

---
+++ module2.4.py
@@ -0,0 +1,27 @@
+from itertools import filterfalse
+
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i == 1:
+        continue
+    is_prime = True
+    for j in range(2,(i-1)):
+        if i % j == 0:
+            is_prime = False
+            break
+    if is_prime == True:
+      primes += [i]
+    else:
+      not_primes += [i]
+
+print('Простые числа: ', primes)
+print('Не простые числа: ', not_primes)
+
+
+
+
+
+
+
