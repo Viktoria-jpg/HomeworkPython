7ab782f95911e3bf90a03650979bb338001c8b64
Viktoria-jpg <black_tea8@bk.ru>
2024-11-16 18:08:49 +0300

    Add files via upload

---
+++ homework16.11.24.py
@@ -0,0 +1,8 @@
+my_list = [42,69,322,13,0,99,-5,9,8,7,-6,5]
+point = 0
+if len(my_list) >= point:
+    while (my_list[point]) >= 0:
+        print(my_list[point])
+        point = point + 1
+        continue
+
