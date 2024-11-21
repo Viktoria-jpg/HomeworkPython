7b964680e1e8bde17d199121bd073a29192e2a46
Viktoria-jpg <black_tea8@bk.ru>
2024-11-21 12:59:12 +0300

    Update module2_hard.py

--- module2_hard.py
+++ module2_hard.py
@@ -10,12 +10,9 @@ print('Число на первом каменном поле: ', on_stone)
 
 result = []
 for i in range(1,20):
-    for j in range(1,20):
+    for j in range(i + 1,20):
         if on_stone % (i+j) == 0:
-            result.append(i)
-            result.append((j))
+            result.append(str(i) + str(j))
 
 
 print("Возможные карианты для второго поля: ", *result)
-
-
