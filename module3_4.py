0e63b22b45c9d7c0765b149948c52fb767276a65
Viktoria-jpg <black_tea8@bk.ru>
2024-12-02 10:08:29 +0300

    Update module3_4.py

--- module3_4.py
+++ module3_4.py
@@ -4,13 +4,14 @@ def single_root_words(root_word, *other_words):
     other_words = list(other_words)
     for number in range(len(other_words)):
         ind = 0
-        for letter in (other_words[number]):
+        for letter in (other_words[number]).lower():
             for check in (root_word):
                 if letter == check:
                     ind = ind + 1
-                    if ind == len(root_word):
+                    if ind == len(root_word) or ind == len(other_words[number]):
                         same_words.append(other_words[number])
                         break
+
     return same_words
 
 print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', ' richies'))
