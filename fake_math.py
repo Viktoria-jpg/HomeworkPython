aa9943a7366b43831a5b12b42842ad4a2a9b410f
Viktoria-jpg <black_tea8@bk.ru>
2024-12-02 15:23:58 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second != 0:
+        return print(first/second)
+    else:
+        print("Ошибка")
---
+++ true_math.py
@@ -0,0 +1,6 @@
+from math import inf
+def divide(first, second):
+    if second != 0:
+        return print(first/second)
+    else:
+        return inf
