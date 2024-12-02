091710189c8b538603cb08362bf39b031f6071c4
Viktoria-jpg <black_tea8@bk.ru>
2024-12-02 09:41:50 +0300

    Add files via upload

---
+++ module3_4.py
@@ -0,0 +1,17 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word.lower()
+    other_words = list(other_words)
+    for number in range(len(other_words)):
+        ind = 0
+        for letter in (other_words[number]):
+            for check in (root_word):
+                if letter == check:
+                    ind = ind + 1
+                    if ind == len(root_word):
+                        same_words.append(other_words[number])
+                        break
+    return same_words
+
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', ' richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
