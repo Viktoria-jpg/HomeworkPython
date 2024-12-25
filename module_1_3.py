4f4f7fe57e9b66ea446c070445d85e7a603f6b76
Viktoria-jpg <black_tea8@bk.ru>
2024-12-25 10:42:31 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,23 @@
+from builtins import PythonFinalizationError
+from operator import truediv
+
+name = "Viktoria"
+print("name:",name)
+
+Age = 22
+print("age:",Age)
+
+Age = Age+1
+print("New age:",Age)
+
+is_student = True
+print("Is Student:",is_student)
+
+
+Homework = 12
+Time = 1.5
+Name = "Python"
+print('Курс:', Name, " Всего задач:", Homework, " Затрачено часов:", Time, ' Среднее время выполнения: ', Time/Homework)
+
+
+
